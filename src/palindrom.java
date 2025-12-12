a2daae64f81a6f50bc289f0e6c309a902286ae95
Gulshan Kumar <gulshangulatimtsn181@gmail.com>
2025-12-12 11:21:35 +0530

    init commit

---
+++ src/palindrom.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class palindrom {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int num = sc.nextInt();
+        int temp = num;
+        int rev = 0;
+        while(temp != 0){
+            int digit = temp%10;
+            rev = rev*10 + digit;
+            temp = temp/10;
+        }
+        if(rev == num){
+            System.out.println("Yes Palindrome");
+        }else{
+            System.out.println("Not a palindrome");
+        }
+    }
+}
