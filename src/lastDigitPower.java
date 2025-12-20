60927d38ea5e145dc5a7f11794494a55244ebd86
Gulshan Kumar <gulshangulatimtsn181@gmail.com>
2025-12-20 23:20:23 +0530

    init commit

---
+++ src/lastDigitPower.java
@@ -0,0 +1,8 @@
+public class lastDigitPower {
+    public static void main(String[] args) {
+        int n = 3;
+        int m = 2;
+        int ans = (int)Math.pow(n,m);
+        System.out.println(ans);
+    }
+}
