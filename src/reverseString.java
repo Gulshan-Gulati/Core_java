34dac8a48e86b12d72e7667547d4a6b65c4c4515
Gulshan Kumar <gulshangulatimtsn181@gmail.com>
2025-12-20 23:20:13 +0530

    init commit

--- src/reverseString.java
+++ src/reverseString.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class reverseString {
     public static void main(String[] args) {
-        String str = "abcd";
+        String str = "abcd efgh";
         char[] arr = str.toCharArray();
         int low = 0;
         int high = arr.length-1;
