548dd5c98fdf576af083d71d50fd7d389fe41da3
Gulshan Kumar <gulshangulatimtsn181@gmail.com>
2025-12-27 20:38:52 +0530

    init commit

---
+++ src/vowelConsonant.java
@@ -0,0 +1,21 @@
+public class vowelConsonant {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        char[] arr = str.toCharArray();
+        int n = arr.length;
+        int countVowel = 0;
+        int countConsonent = 0;
+        for(int i = 0; i<n; i++){
+            if(arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'o' || arr[i] == 'u'){
+                countVowel++;
+            }else if(arr[i] == ' '){
+                continue;
+            }
+            else{
+                countConsonent++;
+            }
+        }
+        System.out.println(countVowel);
+        System.out.println(countConsonent);
+    }
+}
