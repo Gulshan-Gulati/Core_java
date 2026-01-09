e2eb100a9516de9a7ef8cf02c2c7fd1e06c99fe9
Gulshan Kumar <gulshangulatimtsn181@gmail.com>
2026-01-09 23:49:57 +0530

    init commit

---
+++ src/fiboNum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class fiboNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        if(num == 1){
+            System.out.print(1+" ");
+        }else if(num == 2) {
+            System.out.print(a + " " + b);
+        }
+        else if(num > 2) {
+            for (int i = 0; i < num; i++) {
+                int temp = a + b;
+                System.out.print(temp + " ");
+                a = b;
+                b = temp;
+            }
+        }
+    }
+}
