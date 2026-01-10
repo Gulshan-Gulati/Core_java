14a480c0301d367d12661d2d49348b073eb05303
Gulshan Kumar <gulshangulatimtsn181@gmail.com>
2026-01-10 19:26:47 +0530

    init commit

---
+++ src/palinString.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class palinString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String: ");
+        String str = sc.next();
+        char[] arr = str.toCharArray();
+        int low = 0;
+        int high = arr.length-1;
+        while(low <= high){
+            char temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+        System.out.println("Reversed String: ");
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
