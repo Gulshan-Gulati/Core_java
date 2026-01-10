9779a83b3022ce74e7c3c4c7069d108a5feb8378
Gulshan Kumar <gulshangulatimtsn181@gmail.com>
2026-01-10 19:32:46 +0530

    init commit

---
+++ src/palinStr.java
@@ -0,0 +1,17 @@
+public class palinStr {
+    public static void main(String[] args) {
+        String str = "Hello";
+        int n = str.length();
+        int low = 0;
+        int high = n-1;
+        while(low != high){
+            if(str.charAt(low) != str.charAt(high)){
+                System.out.println("Not Palindrome");
+                return;
+            }
+            low++;
+            high--;
+        }
+        System.out.println("Palindrome String");
+    }
+}
